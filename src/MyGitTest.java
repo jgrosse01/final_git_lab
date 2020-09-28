9edb5a761d15d037feffc0470e843c5af98f9bd0
Jacob Grosse <jgrosse01@carroll.edu>
2020-09-28 14:34:38 -0600

    Green improvements

--- src/MyGitTest.java
+++ src/MyGitTest.java
@@ -1,6 +1,6 @@
 
 public class MyGitTest {
 	public static void main(String[] args) {
-		System.out.println("Jake says Hello from master and branch AAAA");
+		System.out.println("Jake has made improvements");
 	}
 }
